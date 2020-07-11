2136b3e8c7a6d0c684eaf5b721319037f123fd3c
indrysfa <indrysfa@gmail.com>
2020-07-11 18:02:32 +0700

    pertemuan20 200711

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# list-karyawan-php
-Daftar Karyawan dengan PHP dengan data array
+# php-data-array-sql
+-Menampilkan Object Dalam Array
+-Form Input menggunakan method $_POST
+-Menampilkan data dalam array
+-Menampilkan data array dalam array
+-Menampilkan Data dari Database SQL
--- project.php
+++ project.php
@@ -42,6 +42,24 @@ $alamat = "Cikokol";
 $gaji = 8000000;
 $img = "gambar1.jpeg";
 
+// echo "<table>
+// <th>NIK</th>
+// <th>Nama</th>
+// <th>Bagian</th>
+// <th>Alamat</th>
+// <th>Gaji</th>
+// <th>Foto</th>
+// </table><br>";
+
+// echo "<tr>
+// <td>$nik</td>
+// <td>$nama</td>
+// <td>$bagian</td>
+// <td>$alamat</td>
+// <td>$gaji</td>
+// <td><img src='img/$img' width='140' height='90' /></td>
+// </tr><br>";
+
 print "NIK: $nik<br>Nama: $nama<br>Bagian: $bagian<br>Alamat: $alamat". "<br>";
 print "Gaji Kotor: Rp." . number_format($gaji, 2) . "<br>";
 echo "<img src='img/$img' width='140' height='90' />";
